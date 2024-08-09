a78650ee048baa86cd6391a79ade6d72b83143dd
RiddhiLokhande <riddhilokhande7@gmail.com>
2024-08-09 12:32:19 +0530

    Update EXP 7 H.cpp

--- EXP 7 H.cpp
+++ EXP 7 H.cpp
@@ -7,12 +7,12 @@ int main() {
     cout << "Enter a string: "; 
     cin >> a; 
     int n = a.length();
-    bool isPalindrome = true;  // Assume the string is a palindrome
+    bool isPalindrome = true;
     
     for(int i = 0; i < n / 2; i++) { 
         if(a[i] != a[n - i - 1]) { 
-            isPalindrome = false;  // If any character doesn't match, it's not a palindrome
-            break;  // No need to check further
+            isPalindrome = false;
+            break;
         } 
     } 
     
