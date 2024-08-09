8d6c16d08e805524a62ccf07ef685a201ae4b9ff
RiddhiLokhande <riddhilokhande7@gmail.com>
2024-08-09 10:54:19 +0530

    Add files via upload

---
+++ EXP 7 E.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    string a;
+    cout<<"enter any word:";
+    cin>>a;
+    cout<<"entered string is "<<a;
+    return 0;
+
+}
